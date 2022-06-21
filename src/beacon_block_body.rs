4781a292d77aa4d2a20ea1e4c5594c9232d4544d
Olga Kunyavsksya <oche24912@yandex.ru>
2022-06-21 13:45:55 +0300

    generate proof

--- src/beacon_block_body.rs
+++ src/beacon_block_body.rs
@@ -15,6 +15,8 @@ use std::env;
 use std::fs;
 use serde_derive::{Deserialize, Serialize};
 use tree_hash_derive::TreeHash;
+use ethereum_types::H256;
+use merkle_proof::MerkleTree;
 
 #[derive(Deserialize, Serialize, TreeHash)]
 pub struct BeaconBlockBody {
@@ -28,6 +30,26 @@ pub struct BeaconBlockBody {
     pub voluntary_exits: VariableList<SignedVoluntaryExit, U16>,
     pub sync_aggregate: SyncAggregate,
     pub execution_payload: ExecutionPayload,
+}
+
+impl BeaconBlockBody {
+    pub fn get_leaves(&self) -> [H256; 10] {
+       use tree_hash::TreeHash;
+
+       let mut llist : [H256; 10] = [
+           self.randao_reveal.tree_hash_root(), 
+           self.eth1_data.tree_hash_root(),
+           self.graffiti.tree_hash_root(),
+           self.proposer_slashings.tree_hash_root(),
+           self.attester_slashings.tree_hash_root(),
+           self.attestations.tree_hash_root(),
+           self.deposits.tree_hash_root(),
+           self.voluntary_exits.tree_hash_root(),
+           self.sync_aggregate.tree_hash_root(),
+           self.execution_payload.tree_hash_root()];
+
+       llist
+    }
 }
 
 #[cfg(test)]
@@ -71,6 +93,37 @@ mod tests {
         println!("{:?}", bbody.tree_hash_root());
 
         assert_eq!(format!("{:?}", bbody.tree_hash_root()), "0x1906eb5417e2ff500a7c48c0704138b340f7d04c9ce7df8d213ad35232a4ff60");
+    }
+
+    #[test]
+    fn build_merkle_tree() {
+        let mut path_exmp_json = std::env::current_exe().unwrap();
+        path_exmp_json.pop();
+        path_exmp_json.push("../../..");
+        path_exmp_json.push("source");
+        path_exmp_json.push("body_exmp2.json");
+
+        let json_str = std::fs::read_to_string(path_exmp_json).expect("Unable to read file");
+
+        let bbody : crate::beacon_block_body::BeaconBlockBody = serde_json::from_str(&json_str).unwrap();
+
+        use tree_hash::TreeHash;
+        println!("{:?}", bbody.tree_hash_root());
+
+        assert_eq!(format!("{:?}", bbody.tree_hash_root()), "0x1906eb5417e2ff500a7c48c0704138b340f7d04c9ce7df8d213ad35232a4ff60");
+
+        let mut leaves = bbody.get_leaves();
+
+        let mtree = merkle_proof::MerkleTree::create(&leaves, 4);
+
+        assert_eq!(mtree.hash(), bbody.tree_hash_root());
+
+        let mut proofg = mtree.generate_proof(1, 4);
+
+        println!("{:?}", proofg);
+
+        assert_eq!(bbody.eth1_data.tree_hash_root(), proofg.0);
+
     }
 }
 
